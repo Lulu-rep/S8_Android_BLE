bf3ceb7e50dd74b9e0f9cfc2573f6b99a10a0a98
Lulu-rep <reppl.luc@gmail.com>
2025-03-24 12:20:15 +0100

    Now redirect to device screen

---
+++ app/src/main/java/fr/isen/repplinger/androidsmartdevice/DeviceDetailActivity.kt
@@ -0,0 +1,26 @@
+package fr.isen.repplinger.androidsmartdevice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import fr.isen.repplinger.androidsmartdevice.ui.theme.AndroidSmartDeviceTheme
+import fr.isen.repplinger.androidsmartdevice.views.DeviceDetailScreen
+
+class DeviceDetailActivity: ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidSmartDeviceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DeviceDetailScreen(modifier = Modifier.padding(innerPadding));
+                }
+            }
+        }
+    }
+}
